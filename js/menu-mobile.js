23c84bc323c963eb7059bef8ed14cb9f80df0d5c
Isabel Alves <66089395+isalves02@users.noreply.github.com>
2022-01-30 21:49:12 -0300

    Delete js directory

--- js/menu-mobile.js
+++
@@ -1,9 +0,0 @@
-// menu hamburger
-const btnMobile = document.getElementById('btn-mobile');
-
-function toggleMenu() {
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active-menu');
-}
-
-btnMobile.addEventListener('click', toggleMenu);
